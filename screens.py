ac2c0bc2853ac6d4727947be301164c710420da5
SpeedyGo55 <83915114+SpeedyGo55@users.noreply.github.com>
2024-11-18 09:16:10 +0100

    added comments

--- screens.py
+++ screens.py
@@ -38,6 +38,7 @@ def leader_board_screen(
     :param leader_board: If the leaderboard screen should be displayed
     :return: A tuple containing the last_response and if the leaderboard screen should be displayed
     """
+    # Render text and get rects
     back_text = game_font.render(
         "Back",
         True,
@@ -48,29 +49,40 @@ def leader_board_screen(
         if event.type == pygame.QUIT or (
             event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
         ):
-            return last_response, False
+            return last_response, False  # Return to the start screen
         mouse_x, mouse_y = pygame.mouse.get_pos()
-        if back_rect.collidepoint(mouse_x, mouse_y):
-            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                return last_response, False
+        if back_rect.collidepoint(
+            mouse_x, mouse_y
+        ):  # If the mouse is over the back button
+            if (
+                event.type == pygame.MOUSEBUTTONDOWN and event.button == 1
+            ):  # If the user clicks the back button
+                return last_response, False  # Return to the start screen
 
     mouse_x, mouse_y = pygame.mouse.get_pos()
     if back_rect.collidepoint(mouse_x, mouse_y):
         back_text = big_game_font.render("Back", True, (0, 255, 255))
         back_rect = back_text.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 190))
 
-    if time.time() - last_response <= 60 * 5:
-        screen.blit(lb_bg, back_rect.scale_by(1.1).topleft, back_rect.scale_by(1.1))
+    if (
+        time.time() - last_response <= 60 * 5
+    ):  # If the last request was made less than 5 minutes ago
+        screen.blit(
+            lb_bg, back_rect.scale_by(1.1).topleft, back_rect.scale_by(1.1)
+        )  # only draw the background where the back button is
         screen.blit(back_text, back_rect)
         pygame.display.flip()
-        return last_response, leader_board
+        return (
+            last_response,
+            leader_board,
+        )  # Return the last response and if the leaderboard screen should be displayed
 
     top_users = get_top_users(5)["dreamlo"]["leaderboard"]["entry"]
     screen.blit(lb_bg, (0, 0))
     y = 0
     for user in top_users:
         user_text = game_font.render(
-            f"{parse.unquote_plus(user['name'])} - {user['score']}",
+            f"{parse.unquote_plus(user['name'])} - {user['score']}",  # Display the user's name and score
             True,
             (0, 255, 255),
         )
@@ -80,9 +92,12 @@ def leader_board_screen(
         y += 53
 
     screen.blit(back_text, back_rect)
-    last_response = time.time()
+    last_response = time.time()  # Update the last response time
     pygame.display.flip()
-    return last_response, leader_board
+    return (
+        last_response,
+        leader_board,
+    )  # Return the last response and if the leaderboard screen should be displayed
 
 
 # noinspection DuplicatedCode
@@ -102,6 +117,8 @@ def start_screen(
     """
     screen.blit(menu_bg, (0, 0))
     events = pygame.event.get()
+
+    # Render text and get rects
     title_text = game_font.render(
         "System Override",
         True,
@@ -124,23 +141,42 @@ def start_screen(
         if event.type == pygame.QUIT or (
             event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
         ):
-            return name_input.value, False, leader_board, False
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            mouse_x, mouse_y = pygame.mouse.get_pos()
-            if start_rect.collidepoint(mouse_x, mouse_y):
-                return name_input.value, True, leader_board, True
-            elif quit_rect.collidepoint(mouse_x, mouse_y):
-                return name_input.value, False, leader_board, False
-            elif leader_board_rect.collidepoint(mouse_x, mouse_y):
-                return name_input.value, started, True, True
-    mouse_x, mouse_y = pygame.mouse.get_pos()
-    if start_rect.collidepoint(mouse_x, mouse_y):
+            return name_input.value, False, leader_board, False  # Quit the game
+        if (
+            event.type == pygame.MOUSEBUTTONDOWN and event.button == 1
+        ):  # If the user clicks the left mouse button
+            mouse_x, mouse_y = pygame.mouse.get_pos()  # Get the mouse position
+            if start_rect.collidepoint(
+                mouse_x, mouse_y
+            ):  # If the user clicks the start button
+                return name_input.value, True, leader_board, True  # Start the game
+            elif quit_rect.collidepoint(
+                mouse_x, mouse_y
+            ):  # If the user clicks the quit button
+                return name_input.value, False, leader_board, False  # Quit the game
+            elif leader_board_rect.collidepoint(
+                mouse_x, mouse_y
+            ):  # If the user clicks the leaderboard button
+                return (
+                    name_input.value,
+                    started,
+                    True,
+                    True,
+                )  # Display the leaderboard screen
+    mouse_x, mouse_y = pygame.mouse.get_pos()  # Get the mouse position
+    if start_rect.collidepoint(
+        mouse_x, mouse_y
+    ):  # If the mouse is over the start button, change the text to big_game_font
         start_text = big_game_font.render("Start", True, (0, 0, 0))
         start_rect = start_text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
-    if quit_rect.collidepoint(mouse_x, mouse_y):
+    if quit_rect.collidepoint(
+        mouse_x, mouse_y
+    ):  # If the mouse is over the quit button, change the text to big_game_font
         quit_text = big_game_font.render("Quit", True, (0, 0, 0))
         quit_rect = quit_text.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 50))
-    if leader_board_rect.collidepoint(mouse_x, mouse_y):
+    if leader_board_rect.collidepoint(
+        mouse_x, mouse_y
+    ):  # If the mouse is over the leaderboard button, change the text to big_game_font
         leader_board_text = big_game_font.render("Leader Board", True, (0, 0, 0))
         leader_board_rect = leader_board_text.get_rect(
             center=(WIDTH // 2, HEIGHT // 2 + 100)
@@ -149,6 +185,7 @@ def start_screen(
     # noinspection PyTypeChecker
     name_input.update(events)
 
+    # Draw everything to the screen
     screen.blit(title_text, title_rect)
     screen.blit(name_input.surface, name_input_rect)
     screen.blit(start_text, start_rect)
@@ -173,6 +210,7 @@ def death_screen(
     global added_user
     screen.blit(death_bg, (0, 0))
 
+    # Render text and get rects
     died_text = you_died_font.render("You Died", True, (255, 0, 0))
     died_rect = died_text.get_rect(center=(WIDTH // 2, HEIGHT // 2 - 150))
 
@@ -189,28 +227,47 @@ def death_screen(
         if event.type == pygame.QUIT or (
             event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
         ):
-            return Player(WIDTH // 2, HEIGHT // 2, 100), running, False, False
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            return (
+                Player(WIDTH // 2, HEIGHT // 2, 100),
+                running,
+                False,
+                False,
+            )  # Quit the game
+        if (
+            event.type == pygame.MOUSEBUTTONDOWN and event.button == 1
+        ):  # If the user clicks the left mouse button
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            if again_rect.collidepoint(mouse_x, mouse_y):
+            if again_rect.collidepoint(
+                mouse_x, mouse_y
+            ):  # If the user clicks the try again button
                 player = Player(
                     WIDTH // 2, HEIGHT // 2, 100, player.high_score, player.name
                 )
                 added_user = False
-                return player, True, False, True
-            elif quit_rect.collidepoint(mouse_x, mouse_y):
-                return player, False, False, False
-            elif leader_board_rect.collidepoint(mouse_x, mouse_y):
-                return player, True, True, False
+                return player, True, False, True  # Start the game
+            elif quit_rect.collidepoint(
+                mouse_x, mouse_y
+            ):  # If the user clicks the quit button
+                return player, False, False, False  # Quit the game
+            elif leader_board_rect.collidepoint(
+                mouse_x, mouse_y
+            ):  # If the user clicks the leaderboard button
+                return player, True, True, False  # Display the leaderboard screen
 
     mouse_x, mouse_y = pygame.mouse.get_pos()
-    if again_rect.collidepoint(mouse_x, mouse_y):
+    if again_rect.collidepoint(
+        mouse_x, mouse_y
+    ):  # If the mouse is over the try again button, change the text to big_game_font
         again_text = big_game_font.render("Try Again", True, (0, 255, 0))
         again_rect = again_text.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 50))
-    if quit_rect.collidepoint(mouse_x, mouse_y):
+    if quit_rect.collidepoint(
+        mouse_x, mouse_y
+    ):  # If the mouse is over the quit button, change the text to big_game_font
         quit_text = big_game_font.render("Quit", True, (0, 255, 0))
         quit_rect = quit_text.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 100))
-    if leader_board_rect.collidepoint(mouse_x, mouse_y):
+    if leader_board_rect.collidepoint(
+        mouse_x, mouse_y
+    ):  # If the mouse is over the leaderboard button, change the text to big_game_font
         leader_board_text = big_game_font.render("Leader Board", True, (0, 255, 0))
         leader_board_rect = leader_board_text.get_rect(
             center=(WIDTH // 2, HEIGHT // 2 + 150)
@@ -218,19 +275,20 @@ def death_screen(
 
     if player.score > player.high_score:
         player.high_score = player.score
-    if not added_user:
-        add_user(player.name, player.score)
+    if not added_user:  # If the user has not been added to the leaderboard
+        add_user(player.name, player.score)  # Add the user to the leaderboard
         added_user = True
 
     score_text = score_font.render(f"Score: {player.score}", True, (0, 255, 0))
     score_rect = score_text.get_rect(center=(WIDTH // 2 - 210, HEIGHT // 2 - 50))
     high_score_text = score_font.render(
         f"High Score: {player.high_score}", True, (0, 255, 0)
-    )
+    )  # Display the player's high score
     high_score_rect = high_score_text.get_rect(
         center=(WIDTH // 2 + 210, HEIGHT // 2 - 50)
     )
 
+    # Draw everything to the screen
     screen.blit(died_text, died_rect)
     screen.blit(score_text, score_rect)
     screen.blit(high_score_text, high_score_rect)
@@ -249,9 +307,11 @@ def get_top_users(top_n: int) -> dict:
     :return: A dictionary containing the top n users
     """
     global LB_PUBLIC, LB_URL
-    url = f"{LB_URL}{LB_PUBLIC}/json/{top_n}"
-    response = requests.get(url)
-    result = json.loads(response.text)
+    url = (
+        f"{LB_URL}{LB_PUBLIC}/json/{top_n}"  # Get the top n users from the leaderboard
+    )
+    response = requests.get(url)  # Make a request to the leaderboard api
+    result = json.loads(response.text)  # Parse the response as json
     return result
 
 
@@ -263,5 +323,5 @@ def add_user(name: str, score: int):
     :return:
     """
     global LB_SECRET, LB_URL
-    url = f"{LB_URL}{LB_SECRET}/add/{parse.quote_plus(name)}/{score}"
-    requests.get(url)
+    url = f"{LB_URL}{LB_SECRET}/add/{parse.quote_plus(name)}/{score}"  # Add the user to the leaderboard
+    requests.get(url)  # Make a request to the leaderboard api
